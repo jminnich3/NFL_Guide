50a43bd8816be9698cc3034edf7b5dbcff1dc04c
jminnich23 <joshminnich3@gmail.com>
2025-05-12 14:50:04 -0400

    Entire project

---
+++ NFLGuide_Proj1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NFLGuide_Proj1
+//
+//  Created by David Zhang on 10/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Let's do it!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NFLGuide_Proj1/NFLGuide_Proj1App.swift
@@ -0,0 +1,17 @@
+//
+//  NFLGuide_Proj1App.swift
+//  NFLGuide_Proj1
+//
+//  Created by David Zhang on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct NFLGuide_Proj1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
